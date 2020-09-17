21158ae2e64cb902fd002fe8dbb995a09131c2e3
Pranjali Gurao <54859364+pranjali5881@users.noreply.github.com>
2020-09-18 01:36:30 +0530

    Add files via upload

---
+++ Back End/app.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const express = require('express')
+const app = express()
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const port = 3003
+mongoose.connect('mongodb://localhost:27017/backendwedding',
+ {useNewUrlParser: true, 
+    useUnifiedTopology: true,
+    useCreateIndex:true
+}).then(()=> {
+    console.log("DB CONNECTED")
+})
+//Middlewares
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+//My routes
+const categoryRoutes = require("./routes/category");
+const authRoutes = require("./routes/auth");
+const productRoutes = require("./routes/product");
+const orderRoutes = require("./routes/order");
+//My Routes
+app.use("/api", categoryRoutes);
+app.use("/api", authRoutes);
+app.use("/api", productRoutes);
+app.use("/api", orderRoutes);
+app.use('/uploads', express.static('uploads'));
+
+app.listen(port, () => console.log(`Wedding app is listening at http://localhost:${port}`))
