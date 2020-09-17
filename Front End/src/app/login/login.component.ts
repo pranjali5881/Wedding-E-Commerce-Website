3b5ea66f08f94e69d7d974e6a7ba9b80eee8f6ab
Pranjali Gurao <54859364+pranjali5881@users.noreply.github.com>
2020-09-18 01:20:49 +0530

    Add files via upload

---
+++ Front End/src/app/login/login.component.ts
@@ -0,0 +1,79 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { Product } from '../product.model';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import {map} from 'rxjs/operators';
+import { Subscription } from 'rxjs';
+import { throwError, BehaviorSubject } from 'rxjs';
+
+import { Userlogin } from '../userlogin.model';
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  //user = new BehaviorSubject<Userlogin>(null);
+  user : any;
+  constructor( private route: ActivatedRoute,
+    private router: Router,private http:HttpClient) { }
+
+  ngOnInit() {
+
+  }
+  
+  loginClicked(form:NgForm)
+  {
+    
+
+    const email = form.value.email;
+    const password = form.value.password;
+
+    const login = { email :email,password:password};
+
+    //alert(password);
+
+    this.http.post('http://localhost:3003/api/signin',login).subscribe(responseData => {
+      console.log(responseData);
+    
+      this.user=responseData;
+    localStorage.setItem('userData', JSON.stringify(this.user.user)); // option 1
+    localStorage.setItem('user_email', this.user.user.email);  // option 2
+    localStorage.setItem('user_name', this.user.user.name);  // option 3
+    localStorage.setItem('user_id', this.user.user._id);  // otpion 4
+
+    sessionStorage.setItem('userData',this.user.user.name);
+    sessionStorage.setItem('userID',this.user.user._id);
+    console.log(this.user.user.name);
+
+      // if(responseData.user.name)
+    // {
+     //    this.handleAuthentication(responseData.user.email,responseData.user._id,responseData.user.name);
+        
+        this.router.navigate(['/main']);
+
+     // }
+    })
+    form.reset();
+  }
+
+  private handleAuthentication(
+    email: string,
+    userId: string,
+    name: string,
+  ) 
+  {
+   
+    const user = new Userlogin(email, userId, name);
+    this.user.next(user);
+
+    localStorage.setItem('userData', JSON.stringify(user)); // option 1
+    localStorage.setItem('user_email', email);  // option 2
+    localStorage.setItem('user_name', name);  // option 3
+    localStorage.setItem('user_id', userId);  // otpion 4
+ 
+    sessionStorage.setItem('user_name', name);
+  }
+
+}
