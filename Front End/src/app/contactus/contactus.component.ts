6f217c72ece2be390c4934589feaa9bca5b3c3d1
Pranjali Gurao <54859364+pranjali5881@users.noreply.github.com>
2020-09-18 01:17:09 +0530

    Add files via upload

---
+++ Front End/src/app/contactus/contactus.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contactus',
+  templateUrl: './contactus.component.html',
+  styleUrls: ['./contactus.component.css']
+})
+export class ContactusComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+  myFunction() {
+    alert("Your Query is Submitted!!! We will get back to you soon");
+  }
+
+}
+
